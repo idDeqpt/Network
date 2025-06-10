b26509833cbd757738c7250bfdcc65787862f764
Владислав Ушаков <ushakov_vl@list.ru>
2025-06-10 14:02:33 +0700

    Fix typo

--- src/TCPServer.cpp
+++ src/TCPServer.cpp
@@ -177,7 +177,7 @@ void net::TCPServer::listen_handler()
         if (select_result > 0)
         {
             int client_socket = accept(this->listen_socket, NULL, NULL);
-            listen_pool.addTask(&TCPServer::client_handler, this, client_socket) << std::endl
+            listen_pool.addTask(&TCPServer::client_handler, this, client_socket);
         }
     }
 }
